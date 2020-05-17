ee23d798d664520629ea92e6be21ece1bcc2b63b
Tech Reagan <reaganekhameye@gmail.com>
2020-05-17 21:05:06 +0100

    Create get and post contacts

--- controllers/contacts.ts
+++ controllers/contacts.ts
@@ -1,26 +1,78 @@
+import Contact from '../models/Contact.ts'
+
 // @desc    Get contacts
 // @route   GET /api/v1/contacts
 // @access  Public
 const getContacts = async (req: any) => {
-  const contacts = [
-    {
-      firstName: 'tech',
-      lastName: 'reagan',
-      phoneNumber: 123456
-    },
-    {
-      firstName: 'john',
-      lastName: 'doe',
-      phoneNumber: 123456
-    }
-  ]
+  // const contacts = [
+  //   {
+  //     firstName: 'tech',
+  //     lastName: 'reagan',
+  //     phoneNumber: 123456
+  //   },
+  //   {
+  //     firstName: 'john',
+  //     lastName: 'doe',
+  //     phoneNumber: 123456
+  //   }
+  // ]
+  const contacts = await Contact.find()
   await req.respond({
     status: 200,
     headers: new Headers({
       'content-type': 'application/json'
     }),
-    body: JSON.stringify(contacts)
+    body: JSON.stringify({ success: true, data: contacts })
   })
 }
 
-export { getContacts }
+// @desc    Get contacts
+// @route   GET /api/v1/contacts
+// @access  Public
+const getContact = async (req: any) => {
+  // const contacts = [
+  //   {
+  //     firstName: 'tech',
+  //     lastName: 'reagan',
+  //     phoneNumber: 123456
+  //   },
+  //   {
+  //     firstName: 'john',
+  //     lastName: 'doe',
+  //     phoneNumber: 123456
+  //   }
+  // ]
+  const contacts = await Contact.find()
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      'content-type': 'application/json'
+    }),
+    body: JSON.stringify({ success: true, data: contacts })
+  })
+}
+
+// @desc    Create contacts
+// @route   POST /api/v1/contacts
+// @access  Public
+const createContact = async (req: any) => {
+  const data = (await req.json()) as {
+    firstName: string
+    lastName: string
+    phoneNumber: number
+  }
+
+  let contact = await Contact.insertOne(data)
+  console.log(contact.$oid)
+  contact = await Contact.findOne({ _id: contact })
+
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      'content-type': 'application/json'
+    }),
+    body: JSON.stringify({ success: true, data: contact })
+  })
+}
+
+export { getContacts, getContact, createContact }
--- routes/contacts.ts
+++ routes/contacts.ts
@@ -1,9 +1,20 @@
-import { createRouter } from 'https://servestjs.org/@v1.0.0/mod.ts'
+import {
+  createRouter,
+  contentTypeFilter
+} from 'https://servestjs.org/@v1.0.0/mod.ts'
 
-import { getContacts } from '../controllers/contacts.ts'
+import {
+  getContacts,
+  getContact,
+  createContact
+} from '../controllers/contacts.ts'
 
 const router = createRouter()
 
 router.get('/', getContacts)
+
+router.get('/:id', getContact)
+
+router.post('/', contentTypeFilter('application/json'), createContact)
 
 export default router
