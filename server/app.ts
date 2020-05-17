9bcb797810e03d417c4b50b9d9fcf2452c7a4602
Tech Reagan <reaganekhameye@gmail.com>
2020-05-17 23:35:00 +0100

    Removed comments from app.ts

--- server/app.ts
+++ server/app.ts
@@ -6,16 +6,6 @@ const app = createApp()
 
 app.route('/api/v1/contacts', contactRoutes)
 
-// app.get('/', async (req) => {
-//   await req.respond({
-//     status: 200,
-//     headers: new Headers({
-//       'content-type': 'application/json'
-//     }),
-//     body: JSON.stringify({ name: 'hello' })
-//   })
-// })
-
 const port = 4002
 
 app.listen({ port })
